3b5d32956cd6c5c7dbc9b9882ce5024e83b84ff3
YasinAkturk <63852309+YasinAkturk@users.noreply.github.com>
2021-10-02 18:51:44 +0300

    Ders1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uyg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adınız Nedir?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Soyadınız Nedir?");
+            var surname = Console.ReadLine();
+            Console.WriteLine("Merhaba "+name+" "+surname);
+            Console.Write($"{Environment.NewLine}Kapatmak için bir tuşa basınız...");
+            Console.ReadKey(true);
+        }
+    }
+}
